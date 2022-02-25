05e9a65fb59476e88ce1e0314bde7a0918c1d557
Syamsu Rijal Efendi <syamsuefendi@gmail.com>
2022-02-25 07:34:00 +0700

    Update Tugas 13

--- tugas-harian-part-2/src/App.js
+++ tugas-harian-part-2/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Tugas10 from "./Tugas-10/tugas10";
 import Tugas11 from "./Tugas-11/tugas11";
 import Tugas12 from "./Tugas-12/tugas12";
-import DataMahasiswa from "./Tugas-13/dataMahasiswa";
+import Tugas13 from "./Tugas-13/tugas13";
 
 const App = () => {
   return (
@@ -10,7 +10,7 @@ const App = () => {
       {/* <Tugas10 /> */}
       {/* <Tugas11 /> */}
       {/* <Tugas12 /> */}
-      <DataMahasiswa />
+      <Tugas13 />
     </>
   );
 };
